4f29c286c82b64046568c483ccca9dfef452f5d3
antoniofortis <antoniofortis@gmail.com>
2022-01-27 02:14:59 -0300

    code madrugada

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,20 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const STATE_INITIAL = {
+  email: '',
+  password: '',
+};
+
+function user(state = STATE_INITIAL, action) {
+  switch (action.type) {
+  case 'USER_ADD':
+    return {
+      ...state,
+      email: action.state.email,
+      password: action.state.password,
+    };
+  default:
+    return state;
+  }
+}
+
+export default user;
